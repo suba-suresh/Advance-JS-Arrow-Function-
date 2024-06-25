383a4ad47a3b6f38e6b0d87a796b85b7ce7a6174
Suba Suresh <subasuresh8095@gmail.com>
2024-06-25 12:00:08 +0000

    arrow function practise

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,29 @@
  */
 
 // Vanilla JavaScript Function
-
+ const  addTwoNumbers = (a, b) => {
+    // code block
+    return a+b;
+ }
+ let sum = addTwoNumbers(3,5)
+ console.log(sum);
 // Arrow Function With Parameters
 
 // Single Line Arrow Function With Parameters
+const  addTwoNumbers2 = (a, b) => a+b;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello there!");
+
+const sayHello = () =>console.log("Hello!");
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+
+   '<p>this is a multiline string! </p>'
+
+
+)
+console.log(returnMultipleLines());
